de43f9a40b8cc4cea21309917f87664a28112c5d
ChetanGavanali <chetangavanali@gmail.com>
2022-07-07 22:17:45 +0530

    Added Email Samples

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,10 +5,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+
     public static void checkValidFirstName(String firstName) {
 
         boolean isFirstName;
-        String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
+        String firstNameRegex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern patternObject = Pattern.compile(firstNameRegex);
         if (firstName == null) {
             isFirstName = false;
@@ -17,15 +18,15 @@ public class UserRegistration {
         isFirstName = matcherObject.matches();
 
         if (isFirstName)
-            System.out.println(firstName + " is Valid ");
+            System.out.println(firstName + " is Valid");
         else
-            System.err.println(firstName + " is Invalid Format");
+            System.out.println(firstName + " is Invalid Format");
     }
 
     public static void checkValidLastName(String lastName) {
 
         boolean isLastName;
-        String lastNameRegex = "[A-Z][a-z]{3,}";
+        String lastNameRegex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern patternObject = Pattern.compile(lastNameRegex);
         if (lastName == null) {
             isLastName = false;
@@ -34,10 +35,11 @@ public class UserRegistration {
         isLastName = matcherObject.matches();
 
         if (isLastName)
-            System.out.println(lastName + " is Valid");
+            System.out.println(lastName + " is Valid ");
         else
-            System.err.println(lastName + " is Invalid Format");
+            System.out.println(lastName + " is Invalid Format");
     }
+
     public static void checkValidEmail(String email) {
 
         boolean isEmail;
@@ -52,13 +54,14 @@ public class UserRegistration {
         if (isEmail)
             System.out.println(email + " is a Valid Email address");
         else
-            System.out.println(email + " is Invalid Format");
+            System.out.println(email + " is Invalid Format ");
     }
+
     public static void checkValidMobileNumber(String mobileNumber) {
 
         boolean isMobileNumber;
-        String mobileNumberRegex = "^[0-9]{2}[ ]{1}[0-9]{10}$";
-        Pattern patternObject = Pattern.compile(mobileNumberRegex);
+        String mobileRegex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+        Pattern patternObject = Pattern.compile(mobileRegex);
         if (mobileNumber == null) {
             isMobileNumber = false;
         }
@@ -68,12 +71,13 @@ public class UserRegistration {
         if (isMobileNumber)
             System.out.println(mobileNumber + " is a Valid  Mobile Number");
         else
-            System.out.println(mobileNumber + " is Invalid Format");
+            System.out.println(mobileNumber + " is Invalid Mobile Number");
     }
+
     public static void checkValidPassword(String password) {
 
         boolean isPassword;
-        String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]*([@])*).{8,}";
+        String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$";
         Pattern patternObject = Pattern.compile(passwordRegex);
         if (password == null) {
             isPassword = false;
@@ -84,34 +88,107 @@ public class UserRegistration {
         if (isPassword)
             System.out.println(password + " is a Valid  Password");
         else
-            System.out.println(password + " Invalid Password");
+            System.out.println(password + " is Invalid Password");
     }
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        System.out.println("Welcome To User Registration");
         Scanner scannerObject = new Scanner(System.in);
 
         System.out.println("Enter Your First Name");
-        String firstName = scannerObject.next();
+        String firstName = scannerObject.nextLine();
         checkValidFirstName(firstName);
 
         System.out.println("Enter Your Last Name");
-        String lastName = scannerObject.next();
+        String lastName = scannerObject.nextLine();
         checkValidLastName(lastName);
 
         System.out.println("Enter Your Email Address");
-        String email = scannerObject.next();
+        String email = scannerObject.nextLine();
         checkValidEmail(email);
 
         System.out.println("Enter Your Mobile Number");
-        String mobileNumber = scannerObject.next();
+        String mobileNumber = scannerObject.nextLine();
         checkValidMobileNumber(mobileNumber);
 
         System.out.println("Enter Your Password");
-        String password = scannerObject.next();
+        String password = scannerObject.nextLine();
         checkValidPassword(password);
 
         scannerObject.close();
+
+        System.out.println("Valid Emails");
+
+        email = "abc@bridgelabz.co.in";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.com";
+        checkValidEmail(email);
+
+        email = "abc@yahoo.com";
+        checkValidEmail(email);
+
+        email = "abc@1.com";
+        checkValidEmail(email);
+
+        email = "abc-100@yahoo.com";
+        checkValidEmail(email);
+
+        email = "abc.100@yahoo.com";
+        checkValidEmail(email);
+
+        email = "abc111@abc.com";
+        checkValidEmail(email);
+
+        email = "abc-100@abc.net";
+        checkValidEmail(email);
+
+        email = "abc.100@abc.com.au";
+        checkValidEmail(email);
+
+        email = "abc+100@gmail.com";
+        checkValidEmail(email);
+
+        System.out.println("Invalid Emails");
+
+        email = ".abc@abc.com";
+        checkValidEmail(email);
+
+        email = "abc";
+        checkValidEmail(email);
+
+        email = "abc@.com.my";
+        checkValidEmail(email);
+
+        email = "abc@abc@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc()*@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc..2002@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc.@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc123@.com";
+        checkValidEmail(email);
+
+        email = "abc123@.com.com";
+        checkValidEmail(email);
+
+        email = "abc123@gmail.a";
+        checkValidEmail(email);
+
+        email = "abc@%*.com";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.1a";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.aa.au";
+        checkValidEmail(email);
+
     }
 }
